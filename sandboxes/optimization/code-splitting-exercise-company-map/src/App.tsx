bc3194b3c0f51d368758c3b4fcb7945e7e130a5d
Gabriel Pichot <gabriel.pichot@gmail.com>
2022-12-08 15:49:24 +0100

    feat: add toggle

--- sandboxes/optimization/code-splitting-exercise-company-map/src/App.tsx
+++ sandboxes/optimization/code-splitting-exercise-company-map/src/App.tsx
@@ -14,19 +14,25 @@ const icon = new L.Icon({
 
 function DetailPage() {
   const location: LatLngExpression = [47.2154556, -1.5644531];
+  const [showMap, setShowMap] = React.useState(false);
   return (
     <>
       <h1>Company</h1>
       <address>Nantes</address>
       <br />
-      <MapContainer
-        center={location}
-        zoom={10}
-        style={{ width: "100%", height: 500 }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={location} icon={icon} />
-      </MapContainer>
+      <button onClick={() => setShowMap(!showMap)}>
+        {showMap ? "Hide" : "Show"} Map
+      </button>
+      {showMap && (
+        <MapContainer
+          center={location}
+          zoom={10}
+          style={{ width: "100%", height: 500 }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={location} icon={icon} />
+        </MapContainer>
+      )}
     </>
   );
 }
